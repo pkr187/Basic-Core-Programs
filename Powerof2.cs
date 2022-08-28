7f89dff861ef5576e42ff7bf94c4876f137585cf
pkr187 <pankajroman187@gmail.com>
2022-08-29 00:05:47 +0530

    Powerof2

---
+++ Powerof2.cs
@@ -0,0 +1,16 @@
+﻿namespace Power_of_2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(ispowertwo(8));
+            Console.ReadKey();
+        }
+        static bool ispowertwo(ulong input)
+        {
+            return ((input & (input - 1)) == 0);
+        }
+        
+    }
+}
