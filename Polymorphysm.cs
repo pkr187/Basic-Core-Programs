7ab1764550431395019ca3b7e216d77dba79b86d
pkr187 <pankajroman187@gmail.com>
2022-08-29 00:08:50 +0530

    Polymorphysm

---
+++ Polymorphysm.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Interface
+{
+    internal class Polymorphysm
+    {
+        class Animal  // Base class (parent) 
+        {
+            public void animalSound()
+            {
+                Console.WriteLine("The animal makes a sound");
+            }
+        }
+
+        class Pig : Animal  // Derived class (child) 
+        {
+            public void animalSound()
+            {
+                Console.WriteLine("The pig says: wee wee");
+            }
+        }
+
+        class Dog : Animal  // Derived class (child) 
+        {
+            public void animalSound()
+            {
+                Console.WriteLine("The dog says: bow wow");
+            }
+        }
+    }
+}
