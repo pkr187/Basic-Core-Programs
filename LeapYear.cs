d747bc077f2acdc667ded081ac712ca89ce9c22d
pkr187 <pankajroman187@gmail.com>
2022-08-28 23:55:50 +0530

    LeapYear

---
+++ LeapYear.cs
@@ -0,0 +1,28 @@
+﻿namespace LeapYear
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter a number:");
+            int num1 = Convert.ToInt32(Console.ReadLine());
+            if(num1%100 == 0)
+            {
+                if (num1 % 400 == 0)
+                    Console.WriteLine("Leap Year");
+                else
+                    Console.WriteLine("Not a leap year");
+            }
+            else
+            {
+                if (num1 % 4 == 0)
+                    Console.WriteLine("Leap Year");
+                else
+                    Console.WriteLine("Not a leap year");
+            }
+            Console.Read();
+                
+            
+        }
+    }
+}
