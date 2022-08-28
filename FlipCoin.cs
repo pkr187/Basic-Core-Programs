b22a00ed037bb88c5612c30ae92fb12492797fe4
pkr187 <pankajroman187@gmail.com>
2022-08-28 23:49:02 +0530

    FlipCoin

---
+++ FlipCoin.cs
@@ -0,0 +1,38 @@
+﻿namespace FlipCoin
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            // INITIALIZATION
+            Random ran = new Random();
+            int randNum = ran.Next();
+            Console.WriteLine("guess head or tail");
+            string guess = Console.ReadLine();
+            if (randNum == 1 && guess == "Heads")
+            {
+                Console.WriteLine("the coin flip was heads, you win!");
+            }
+            else if (randNum == 2 && guess == "Tails")
+            {
+                Console.WriteLine("the coin flip was Tails, you win:");
+            }
+            else
+            {
+                string result;
+                if (randNum == 1)
+                {
+                    result = "Heads";
+                }
+                else
+                {
+                    result = "Tails";
+                }
+                Console.WriteLine("you lose the coin flip was {0}", result);
+                Console.WriteLine("press any key to quit");
+                Console.ReadKey();
+            }
+            Console.ReadLine();
+        }
+    }
+}
